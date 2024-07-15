6f171d16f5649bccb247f07740353bb76b1cdd84
Kuren Peng <yuanpeng@hoyabit.com>
2024-07-15 20:39:57 +0800

    feat: 1.0

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("listening on *:4000");
+});
