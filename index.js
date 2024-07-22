6a0d5c7cbb3a834f45e445b3aab6ef988f436c02
Kuren Peng <yuanpeng@hoyabit.com>
2024-07-22 18:47:51 +0800

    feat: 2.0

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+const io = socketIo(server);
+
+app.get("/", (req, res) => {
+  res.send("Chat server is running");
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
 });
 
-server.listen(4000, () => {
-  console.log("listening on *:4000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
